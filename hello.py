09a5fa9d104c08741cd67ff0a932fd821729dc1e
Hkp005 <120927183+Hkp005@users.noreply.github.com>
2022-12-18 17:00:05 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,122 @@
+ # print("Hello World!")
+#print("I love india")
+"""
+i am learning python right now 
+ this is something intresting
+ """
+
+
+#print("code is ready!")
+#x = 5       #xis an int
+#y = "hally" #y is str
+#print(x)
+#print(y)
+#fruits = "apple", "Banana", "cherry"
+#x, y, z = fruits
+#print(x)
+#print(y)
+#print(z)
+#x= "Harry Potter"
+#y= " is "
+#z= "great"
+#print(x+y+z)
+#x="Harry"
+#y="potter"
+#z="is small"
+#print(x,y,z)
+#x=5 
+#y=6
+#z=7
+#x= "python is "
+#def myfunc():
+ #print("Awsome" +x)
+#myfunc()
+#x="be"
+#def myfunc():
+ #   x="i am a"
+#    print("a good man" +x)
+#myfunc()
+#print(x+ " a smart man")
+#def myfunc():
+ # x= " my "
+
+#myfunc()
+
+#print("jai mata di" + x)
+#x=67>78
+#print(type(x))
+#=int(4.53)
+#print(x)
+#print(type(x))
+'''
+x=13242
+y=1.8
+z=1j
+a=float(x)
+b=int(y)
+c=complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+'''
+'''
+i=1
+while i<=6:
+ print(i)
+'''
+'''
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1
+'''
+'''
+i=0
+while i<6:
+    i+=1
+    if i==3:
+     continue
+    print(i)
+'''
+'''
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+    print("i is no longer less than 6")
+'''
+'''
+def my_function():
+ print("hello from hritik")
+my_function()    
+'''
+'''
+def my_function(fname):
+    print(fname + "Refences")
+my_function("email")
+my_function("gender")
+my_function("life")
+
+'''
+print("hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
