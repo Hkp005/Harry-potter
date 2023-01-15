d73e28f43c2d09ecf717ecbfcc43d03e571de786
Hritik Pandey <120927183+Hkp005@users.noreply.github.com>
2023-01-16 01:02:59 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+x=(int)(input("enter first number : "))
+y=(int)(input("enter second number : "))
+
+z=x+y
+
+print("Addition is = ",z )
+
+x=(int)(input("enter first number : "))
+y=(int)(input("enter second number : "))
+
+t=x*y
+
+print("Multiplication is = ",t)
+
+x=(float)(input("enter first number : "))
+y=(float)(input("enter second number : "))
+
+j=x//y
+
+print("Division is =",j)
+
+
+
+
+
